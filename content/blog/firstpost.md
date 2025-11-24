2eed314726998220c4ebb63c68a36371ffb49731
Robert Klink <roberthklink@gmail.com>
2025-11-24 17:54:35 +0100

    proj: switch to using 11ty

---
+++ content/blog/firstpost.md
@@ -0,0 +1,8 @@
+---
+title: Welcome!
+description: Hiiii hello hello
+date: 2025-11-24
+tags: misc
+---
+
+Hello hello hi hi
--- src/routes/about/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import Window from '$lib/Window.svelte';
-</script>
-
-<Window title="About">
-    <h1>About</h1>
-    <p>It's me</p>
-</Window>
